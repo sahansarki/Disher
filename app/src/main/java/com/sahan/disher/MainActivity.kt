9112df4ec8813c3931abe440c56567315b8a6a87
sahan <sahancansarki@gmail.com>
2021-11-27 01:49:05 +0300

    First Commit Disher Project

---
+++ app/src/main/java/com/sahan/disher/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.sahan.disher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.sahan.disher.category.CategoryScreen
+import com.sahan.disher.ui.theme.DisherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DisherTheme {
+                DisherApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun DisherApp() {
+    CategoryScreen()
+}
+
---
+++ app/src/main/java/com/sahan/disher/category/CategoryScreen.kt
@@ -0,0 +1,29 @@
+package com.sahan.disher.category
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CategoryScreen(){
+    val categories = getFakeData()
+    Column {
+        Text("Category")
+        Spacer(modifier = Modifier.height(30.dp))
+        categories.forEach {
+            Text(text = it)
+        }
+    }
+}
+
+fun getFakeData(): List<String> {
+    return listOf(
+        "Fish",
+        "Vegan",
+        "Meat"
+    )
+}
